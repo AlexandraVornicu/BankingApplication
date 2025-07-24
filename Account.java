0f71d7260aa64a53ee536b82390cfa4bd4b65e01
AlexandraVornicu <alexandra.i.vornicu@gmail.com>
2025-07-24 12:05:53 +0300

    added deposit, withdraw and getPreviousTransaction methods

--- Account.java
+++ Account.java
@@ -2,12 +2,36 @@ import java.util.Scanner;
 
 public class Account {
     int balance;
-    int previusTransaction;
+    int previousTransaction;
     String customerName;
     String customerID;
 
     Account(String name, String cid) {
         customerName = name;
         customerID = cid;
+    }
+
+    void deposit(int amount) {
+        if (amount != 0) {
+            balance += amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void wirhdraw(int amount) {
+        if (amount != 0) {
+            balance -= amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction occurred.");
+        }
     }
 }
